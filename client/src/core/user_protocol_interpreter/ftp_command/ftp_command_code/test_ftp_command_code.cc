3b0141c58c0a25fb342fa7a5a6060d4242fefc30
liblaf <liblaf@outlook.com>
2022-02-08 19:10:03 +0800

    chore(client): update `test_ftp_command_code`

--- client/src/core/user_protocol_interpreter/ftp_command/ftp_command_code/test_ftp_command_code.cc
+++ client/src/core/user_protocol_interpreter/ftp_command/ftp_command_code/test_ftp_command_code.cc
@@ -7,46 +7,46 @@ using namespace testing;
 TEST(TestFtpCommandCode, TestValidEnum) {
   auto code = FtpCommandCode(FtpCommandCode::kAbor);
   EXPECT_TRUE(code.IsValid());
-  EXPECT_EQ(code.ToEnum(), FtpCommandCode::kAbor);
-  EXPECT_EQ(code.ToString(), "ABOR");
-  EXPECT_EQ(code.Rfc(), "");
-  EXPECT_EQ(code.Description(), "Abort an active file transfer.");
+  EXPECT_EQ(code.GetEnum(), FtpCommandCode::kAbor);
+  EXPECT_EQ(code.GetString(), "ABOR");
+  EXPECT_EQ(code.GetRfc(), "");
+  EXPECT_EQ(code.GetDescription(), "Abort an active file transfer.");
 }
 
 TEST(TestFtpCommandCode, TestValidLowerCase) {
   auto code = FtpCommandCode("abor");
   EXPECT_TRUE(code.IsValid());
-  EXPECT_EQ(code.ToEnum(), FtpCommandCode::kAbor);
-  EXPECT_EQ(code.ToString(), "ABOR");
-  EXPECT_EQ(code.Rfc(), "");
-  EXPECT_EQ(code.Description(), "Abort an active file transfer.");
+  EXPECT_EQ(code.GetEnum(), FtpCommandCode::kAbor);
+  EXPECT_EQ(code.GetString(), "ABOR");
+  EXPECT_EQ(code.GetRfc(), "");
+  EXPECT_EQ(code.GetDescription(), "Abort an active file transfer.");
 }
 
 TEST(TestFtpCommandCode, TestValidUpperCase) {
   auto code = FtpCommandCode("ABOR");
   EXPECT_TRUE(code.IsValid());
-  EXPECT_EQ(code.ToEnum(), FtpCommandCode::kAbor);
-  EXPECT_EQ(code.ToString(), "ABOR");
-  EXPECT_EQ(code.Rfc(), "");
-  EXPECT_EQ(code.Description(), "Abort an active file transfer.");
+  EXPECT_EQ(code.GetEnum(), FtpCommandCode::kAbor);
+  EXPECT_EQ(code.GetString(), "ABOR");
+  EXPECT_EQ(code.GetRfc(), "");
+  EXPECT_EQ(code.GetDescription(), "Abort an active file transfer.");
 }
 
 TEST(TestFtpCommandCode, TestInvalidEnum) {
   auto code = FtpCommandCode(static_cast<FtpCommandCode::Enum>(123456));
   EXPECT_FALSE(code.IsValid());
-  EXPECT_EQ(code.ToEnum(), FtpCommandCode::kUnknownFtpCommandCode);
-  EXPECT_EQ(code.ToString(), "");
-  EXPECT_EQ(code.Rfc(), "");
-  EXPECT_EQ(code.Description(), "");
+  EXPECT_EQ(code.GetEnum(), FtpCommandCode::kUnknownFtpCommandCode);
+  EXPECT_EQ(code.GetString(), "");
+  EXPECT_EQ(code.GetRfc(), "");
+  EXPECT_EQ(code.GetDescription(), "");
 }
 
 TEST(TestFtpCommandCode, TestInvalidString) {
   auto code = FtpCommandCode("ABCD");
   EXPECT_FALSE(code.IsValid());
-  EXPECT_EQ(code.ToEnum(), FtpCommandCode::kUnknownFtpCommandCode);
-  EXPECT_EQ(code.ToString(), "");
-  EXPECT_EQ(code.Rfc(), "");
-  EXPECT_EQ(code.Description(), "");
+  EXPECT_EQ(code.GetEnum(), FtpCommandCode::kUnknownFtpCommandCode);
+  EXPECT_EQ(code.GetString(), "");
+  EXPECT_EQ(code.GetRfc(), "");
+  EXPECT_EQ(code.GetDescription(), "");
 }
 
 // int main(int argc, char *argv[]) {
